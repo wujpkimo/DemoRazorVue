7314ac90a72153f270e1e662a890323f6f6393a2
Jason.Wu <wujpkimo@gmail.com>
2022-06-09 11:31:00 +0800

    加入專案檔案。

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DemoRazorVue.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public string Message { get; set; }
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        Message = "Test msg from backend";
+    }
+
+    public JsonResult OnGetGetOption()
+    {
+        var options = new List<string>()
+        {
+            "Test1","test2","Mini","Mic"
+        };
+        return new JsonResult(options);
+    }
+}
